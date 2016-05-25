c1633e5c524ba4a79e3f846a6e2af2b4c277cf4d
tambui <tam.bui@sunbytes.vn>
2016-05-25 14:00:17 +0700

    messg

---
+++ jb_tax_post_meta.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: JB tax and post meta data
+Plugin URI: http://jbprovider.com
+Description:  JB tax and post meta data is quite possibly the best way  to <strong>create meta value for custom post type and custom taxonomy</strong>.
+Version: 1.0
+Author: JACK BUI
+Author URI: http://jbprovider.com
+License: GPLv2 or later
+Text Domain: jb_theme
+*/
